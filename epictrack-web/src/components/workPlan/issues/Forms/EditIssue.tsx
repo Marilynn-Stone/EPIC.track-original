e8419f8acba1699c9a59a0972ecfaa5c1f07a616
Marilynn Stone <MarilynnStone@proton.me>
2024-12-10 02:42:52 +0000

    sorting of displayed issues

--- epictrack-web/src/components/workPlan/issues/Forms/EditIssue.tsx
+++ epictrack-web/src/components/workPlan/issues/Forms/EditIssue.tsx
@@ -23,7 +23,12 @@ const schema = yup.object().shape({
   is_high_priority: yup.boolean(),
   is_resolved: yup.boolean(),
   start_date: yup.string().required("Start date is required"),
-  expected_resolution_date: yup.string().nullable(),
+  expected_resolution_date: yup.string()
+    .when('is_resolved', {
+      is: true,
+      then: (schema) => schema.required("Missing Resolution Date"),
+      otherwise: (schema) => schema.nullable()
+    }),
 });
 
 const EditIssue = () => {
@@ -170,7 +175,7 @@ const EditIssue = () => {
           />
         </Grid>
         <Grid item xs={6}>
-          <ETFormLabel required>Resolution Date</ETFormLabel>
+          <ETFormLabel >Resolution Date</ETFormLabel>
           <ControlledDatePicker name="expected_resolution_date" />
         </Grid>
       </Grid>
